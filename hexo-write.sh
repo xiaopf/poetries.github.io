209f4ab014deced11100062c222e388013f80515
poetries <jingguanliuye@gmail.com>
2016-07-04 18:06:40 +0800

    modify

--- hexo-write.sh
+++ hexo-write.sh
@@ -1,6 +1,6 @@
-
 rm -rf    ~/桌面/poetries.github.io/
 git clone  -b dev https://github.com/poetries/poetries.github.io.git 
 cd ~/桌面/poetries.github.io
 cp  ~/桌面/poetries.github.io/scaffolds/post.md   ./source/_posts/
-subl  ~/桌面/poetries.github.io/source/_posts/post.md
+subl ~/桌面/poetries.github.io/source/_posts/post.md
+chromium-browser
